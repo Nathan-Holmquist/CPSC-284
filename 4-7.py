8fa12270057dd80789964fa2fde0b73529021eb4
Nathan Holmquist <59553145+Nathan-Holmquist@users.noreply.github.com>
2024-04-09 13:36:36 -0400

    added directions

--- 4-7.py
+++ 4-7.py
@@ -1,2 +1,19 @@
-print("Hello World")
+import numpy as np
+import matplotlib.pyplot as plt
 
+x = np.linspace(-2,4,num = 100)
+
+a = 1
+b = 2
+c = 0
+d = 0
+fx = []
+
+for i in range(len(x)):
+    fx.append( a + b*x[i] + c*x[i]**2 + d*x[i]**3 )
+
+    plt.plot(x,fx)
+    plt.axvline()
+    plt.axhline()
+    plt.plot([-1, 0, 1, 2, 3], [-2, 2, 1, 3, 4], 'bo')
+    plt.shot()
