9b58c7a80b5468e2e66ee2f1cad8fc7c4cd2efe4
Nathan Holmquist <59553145+Nathan-Holmquist@users.noreply.github.com>
2024-04-09 20:41:15 -0400

    Added Answers file and updated 1-3 to be what I am currently working on

--- 1-3.py
+++ 1-3.py
@@ -2,24 +2,7 @@ from sympy import *
 from sympy.interactive.printing import init_printing
 import math
 
+# Just the regular matrix is the "Coeficient Matrix"
 A = Matrix([[2,-1],[1,3],[5,1]])
 print(A)
-
-b = Matrix([[5],[2],[4]])
-print(b)
-
-AugA = A.row_join(b)
-print(AugA)
-
-D = AugA.rref()
-print(D[0])
-
-AT = A.transpose()
-print(AT)
-
-AugAT = AT*AugA
-print(AugAT)
-
-D = AugAT.rref()
-print(D[0])
 
